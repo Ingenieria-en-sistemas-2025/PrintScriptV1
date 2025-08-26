22fd0b08919795399b2684f43bcdd0c5b3163889
catalinapereyra <cpereyra@mail.austral.edu.ar>
2025-08-25 23:30:14 -0300

    .

---
+++ interpreter/src/main/kotlin/Binding.kt
@@ -0,0 +1,6 @@
+package org.example
+
+import Type
+
+//("a" -> Binding(type = NUMBER, value = Num(12.0)))
+data class Binding(val type: Type, val value: Value)
---
+++ interpreter/src/main/kotlin/Value.kt
@@ -0,0 +1,6 @@
+package org.example
+
+sealed interface Value {
+    data class Num(val n : Double) : Value
+    data class Str(val s : String) : Value
+}
