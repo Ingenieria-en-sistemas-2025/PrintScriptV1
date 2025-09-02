0d19c039c6f5a6d576e8d14675b946e55046bfaf
catalinapereyra <cpereyra@mail.austral.edu.ar>
2025-09-02 15:34:04 -0300

    unify build logic into a single convention file

--- ast/src/main/kotlin/Expression.kt
+++ ast/src/main/kotlin/Expression.kt
@@ -1,3 +1,3 @@
 sealed interface Expression { val span: Span }
 
-class InvalidExpression(override val span: Span) : Expression // para test de interpreter
+// class InvalidExpression(override val span: Span) : Expression // para test de interpreter
