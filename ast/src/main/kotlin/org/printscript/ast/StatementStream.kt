c2c768a4dec23072fb8dab648e17ca3b65ea2c12
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-09-12 10:52:40 -0300

    statement stream

--- ast/src/main/kotlin/org/printscript/ast/StatementStream.kt
+++ ast/src/main/kotlin/org/printscript/ast/StatementStream.kt
@@ -1,9 +1,5 @@
 package org.printscript.ast
 
-import org.printscript.common.LabeledError
-import org.printscript.common.Result
-
 interface StatementStream {
-    fun next(): Result<Pair<Statement, StatementStream>, LabeledError>
-    fun isEof(): Boolean
+    fun step(): Step
 }
