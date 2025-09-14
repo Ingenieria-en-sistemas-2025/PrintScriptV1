dbde5ce81a53defab40044a5e01222cd95326553
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-09-14 03:32:43 -0300

    cambios

--- analyzer/src/main/kotlin/org/printscript/analyzer/AnalyzerFactory.kt
+++ analyzer/src/main/kotlin/org/printscript/analyzer/AnalyzerFactory.kt
@@ -1,24 +1,37 @@
 package org.printscript.analyzer
 
+import org.printscript.analyzer.config.AnalyzerConfig
 import org.printscript.analyzer.rules.IdentifierStyleRuleStreaming
 import org.printscript.analyzer.rules.PrintlnSimpleArgRuleStreaming
 import org.printscript.analyzer.rules.ReadInputSimpleArgRuleStreaming
 import org.printscript.common.Version
 
 object AnalyzerFactory {
-    fun forVersion(v: Version): StreamingAnalyzer = when (v) {
-        Version.V0 -> DefaultStreamingAnalyzer(
-            listOf(
-                IdentifierStyleRuleStreaming(),
-                PrintlnSimpleArgRuleStreaming(),
-            ),
-        )
-        Version.V1 -> DefaultStreamingAnalyzer(
-            listOf(
-                IdentifierStyleRuleStreaming(),
-                PrintlnSimpleArgRuleStreaming(),
-                ReadInputSimpleArgRuleStreaming(),
-            ),
-        )
+
+    fun forVersion(v: Version, cfg: AnalyzerConfig): StreamingAnalyzer {
+        val rules = buildList {
+            if (cfg.identifiers.enabled) add(IdentifierStyleRuleStreaming())
+            if (cfg.printlnRule.enabled) add(PrintlnSimpleArgRuleStreaming())
+            if (v == Version.V1 && cfg.readInputRule.enabled) add(ReadInputSimpleArgRuleStreaming())
+        }
+        return DefaultStreamingAnalyzer(rules)
     }
+
+    @Deprecated("Preferir forVersion(version, config)")
+    fun forVersion(v: Version): StreamingAnalyzer =
+        when (v) {
+            Version.V0 -> DefaultStreamingAnalyzer(
+                listOf(
+                    IdentifierStyleRuleStreaming(),
+                    PrintlnSimpleArgRuleStreaming(),
+                ),
+            )
+            Version.V1 -> DefaultStreamingAnalyzer(
+                listOf(
+                    IdentifierStyleRuleStreaming(),
+                    PrintlnSimpleArgRuleStreaming(),
+                    ReadInputSimpleArgRuleStreaming(),
+                ),
+            )
+        }
 }
--- analyzer/src/main/kotlin/org/printscript/analyzer/config/IdentifiersConfig.kt
+++ analyzer/src/main/kotlin/org/printscript/analyzer/config/IdentifiersConfig.kt
@@ -3,6 +3,7 @@ package org.printscript.analyzer.config
 import org.printscript.analyzer.rules.IdentifierStyle
 
 data class IdentifiersConfig(
+    val enabled: Boolean = false,
     val style: IdentifierStyle = IdentifierStyle.CAMEL_CASE,
     val checkReferences: Boolean = false,
     val failOnViolation: Boolean = false, // por defecto solo warning.
