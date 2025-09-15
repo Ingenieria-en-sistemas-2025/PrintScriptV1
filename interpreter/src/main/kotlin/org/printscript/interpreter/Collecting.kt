0b4393e67832b33c11fb3e332d26b7f28c225d7d
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-09-15 19:34:35 -0300

    ..

--- interpreter/src/main/kotlin/org/printscript/interpreter/Collecting.kt
+++
@@ -1,6 +0,0 @@
-package org.printscript.interpreter
-
-data class Collecting(private val lines: List<String>) : Output {
-    override fun append(s: String): Output = Collecting(lines + s)
-    override fun asList(): List<String> = lines
-}
