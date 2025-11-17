63cc37480b99c7678499fbeff583769fb7668f66
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-11-17 11:26:11 -0300

    queue input provider

---
+++ runner/src/main/kotlin/org/printscript/runner/helpers/QueueInputProvider.kt
@@ -0,0 +1,13 @@
+package org.printscript.runner.helpers
+
+import org.printscript.interpreter.InputProvider
+
+class QueueInputProvider(items: List<String>) : InputProvider {
+    private val it = items.iterator()
+    override fun read(prompt: String): String =
+        if (it.hasNext()) {
+            it.next()
+        } else {
+            error("No more inputs available for prompt: $prompt")
+        }
+}
